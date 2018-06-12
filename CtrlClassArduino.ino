3f7044abfe23add483cb4b847a03d64b2ad21f5f
Gustavo-Policarpo <gustavopolicarpo@get.inatel.br>
2018-06-12 15:57:12 -0300

    Update
    -Error state
    -Optimizations

--- CtrlClassArduino.ino
+++ CtrlClassArduino.ino
@@ -1,124 +1,96 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Ethernet.h>
-#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+#include <EthernetUdp.h> // UDP library from: bjoern@cs.stanford.edu 12/30/2008       
 
-#define SS_PIN 49
-#define RST_PIN 48
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+MFRC522 mfrc522(49, 48); 
  
-// Configurações da interface de rede do Arduino
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ipServer(192, 168, 137, 1);
 IPAddress ip(192, 168, 137, 2);
 unsigned int localPort = 8888;
-
-// Configurações do servidor
-IPAddress ipServer(192, 168, 137, 1);
 unsigned int serverPort = 6000;
+EthernetUDP Udp; // An EthernetUDP instance to let us send and receive packets over UDP
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // Buffer to hold incoming packet,
 
-// buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
-
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
-
-void setup() {
-  // config Serial
-  Serial.begin(9600);
-
-  // start the Ethernet and UDP:
-  Ethernet.begin(mac, ip);
-  Udp.begin(localPort);
-  
-  // start RFID
-  SPI.begin();
+void setup(){
+  Serial.begin(9600); // config Serial
+  Ethernet.begin(mac, ip); // start Ethernet
+  Udp.begin(localPort); // start UDP
+  SPI.begin(); // start RFID
   mfrc522.PCD_Init();
-  Serial.println("Aproxime o seu cartao do leitor...\n");
-
-  // config Saida
-  pinMode(14, OUTPUT); 
-  digitalWrite(14, LOW);
-  pinMode(15, OUTPUT); 
-  digitalWrite(15, LOW);
+  pinMode(14, OUTPUT); digitalWrite(14, LOW); // config 14 Saida
+  pinMode(15, OUTPUT); digitalWrite(15, LOW); // config 15 Saida
 }
 
-void sendToServer(String tag) {
-  Serial.print("Sending:  [");
-  Serial.print(tag.c_str());
-  Serial.println("]...");
-  Udp.beginPacket(ipServer, serverPort);
-  Udp.write("123456789AB");
-  //Udp.write(tag.c_str());
+void loop(){
+  Serial.println("Aproxime o seu cartao do leitor...\n");
+  rfid();
+}
+
+void sendToServer(String tag){
+  Serial.print("Sending:  ["); Serial.print(tag.c_str()); Serial.println("]...");
+  Udp.beginPacket(ipServer, serverPort); Udp.write(tag.c_str());
   Udp.endPacket();
 }
 
-bool receiveFromServer() {
-  int packetSize = Udp.parsePacket();
-  if (packetSize) {
+int receiveFromServer(){
+  if (Udp.parsePacket()){
     Serial.print("Received packet from ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i = 0; i < 4; i++) {
-      Serial.print(remote[i], DEC);
-      if (i < 3) {
-        Serial.print(".");
-      }
-    }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
+    for (int i = 0; i < 4; i++) Serial.print( (Udp.remoteIP())[i], DEC);
+    Serial.print(", port "); Serial.println(Udp.remotePort());
 
-    // read the packet into packetBufffer
+    for(int i=0; i<UDP_TX_PACKET_MAX_SIZE; i++) packetBuffer[i] = 0;
     Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.print("Received:  [");
-    Serial.print(packetBuffer);
-    Serial.println("]");
+    Serial.print("Received:  ["); Serial.print(packetBuffer); Serial.println("]");
     
     return strcmp(packetBuffer, "true") == 0;
   }
-  Serial.println("Failed to receive");
-  return false;
+  Serial.println("Failed to receive"); return -1;
 }
 
-bool isAllowed(String tag){ //retorna se o usuario está permitido ou nao
+int isAllowed(String tag){ //retorna se o usuario está permitido ou nao
   Serial.println("Checking if UID is authorized...");
-  sendToServer(tag);
-  delay(500);
+  sendToServer(tag); delay(1000);
   return receiveFromServer();
 }
 
 void check(String tag){
   Serial.println("TAG: " + tag);
-  if(isAllowed(tag)){ // usuario permitido
-    Serial.println("Usuário autorizado");
-    digitalWrite(15, HIGH);
-    digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
-    digitalWrite(14, HIGH);  delay(50); digitalWrite(14, LOW);
-    digitalWrite(15, LOW);
-  }else{
-    Serial.println("Não está autorizado");
-    digitalWrite(14, HIGH); delay(150); digitalWrite(14, LOW); delay(50); 
-    digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
-    digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
-    digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW);
-  }
-  delay(1000);
+  int stat = isAllowed(tag);
+  if(stat == 1) outOk(); // Autorizado
+  if(stat == 0) outNo(); // Não autorizado
+  if(stat == -1) outEr(); // Erro 
+  delay(500);
   Serial.println("**********************\n");
 }
 
 void rfid(){
-  if ( ! mfrc522.PICC_IsNewCardPresent()) return;
-  if ( ! mfrc522.PICC_ReadCardSerial()) return;
+  while ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) delay(100) ;
   String tag= "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     tag.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     tag.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
+  for (byte i = 0; i < mfrc522.uid.size; i++) tag.concat(String(mfrc522.uid.uidByte[i], HEX));
   tag.toUpperCase();
-  check(tag.substring(1));
+  //check(tag.substring(1));
+  check(tag);
 }
 
-void loop() {
-  rfid(); delay(500); // Início do código RFID
-} 
+void outOk(){
+  Serial.println("Usuário autorizado");
+  digitalWrite(15, HIGH);
+  digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
+  digitalWrite(14, HIGH);  delay(50); digitalWrite(14, LOW);
+  digitalWrite(15, LOW);
+}
+
+void outNo(){
+  Serial.println("Não está autorizado");
+  digitalWrite(14, HIGH); delay(150); digitalWrite(14, LOW); delay(50); 
+  digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
+  digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW); delay(50); 
+  digitalWrite(14, HIGH); delay(50); digitalWrite(14, LOW);
+}
+
+void outEr(){
+  Serial.println("ERROR");
+  digitalWrite(14, HIGH); delay(3000); digitalWrite(14, LOW);
+}
